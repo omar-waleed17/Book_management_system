77b7b41b805ef66ad117ded814e88eb75bb002e5
Mazen Azhary <mazen.mazen9850@gmail.com>
2025-12-26 23:17:50 +0200

    admin page

---
+++ ClientSide/js/admindashboard.js
@@ -0,0 +1,25 @@
+// Check if user is admin, redirect to index.html if not
+
+const role = localStorage.getItem("role");
+const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+// Check if user is logged in and has admin role
+if (!isLoggedIn || role?.toLowerCase() !== "admin") {
+  alert("Access denied. Admin privileges required.");
+  window.location.href = "../index.html";
+}
+
+document.getElementById("logoutBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.removeItem("isLoggedIn");
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
+  localStorage.removeItem("currentUser");
+  window.location.href = "../index.html";
+});
+const customerNameElement = document.getElementById("customerName");
+if (username) {
+    customerNameElement.textContent = `Welcome ${username}`;
+  } else {
+    customerNameElement.textContent = "Welcome ADMIN";
+  }
